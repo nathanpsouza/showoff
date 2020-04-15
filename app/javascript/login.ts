abad8799ec089f924e4e284ed1fba3c1d4043a0c
Nathan Souza <nathanpsouza@gmail.com>
2020-04-15 00:27:46 -0300

    feedback for failed login

--- app/javascript/login.ts
+++ app/javascript/login.ts
@@ -1,12 +1,12 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
-  
+$(document).ready(() => {  
   $("#login-form").on("ajax:success", (event) => {
     const [data, status, xhr] = event.detail
     window.location.href = '/'
   }).on("ajax:error", (event) => {
     const [data, status, xhr] = event.detail
-    console.log(data)
+    $('#login-result-message').text(data.data)
+    $('#login-result-message').show()
   })
 })
